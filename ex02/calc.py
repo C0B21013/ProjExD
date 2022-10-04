c17214a2fe0e7ef33301b8710a8c167b0627aecf
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-04 14:32:45 +0900

    2,ボタン作成

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,4 +4,14 @@ import tkinter.messagebox as tkm
 root = tk.Tk()
 root.title("calc")
 root.geometry("300x500")
+
+r, c = 0, 0
+for i, num in enumerate(range(9,-1,-1),1):
+    button=tk.Button(root, text=f"{num}",font=("Ricty Diminished",30),width=4,height=2)
+    button.grid(row = r, column = c)
+    c +=1 
+    if i%3==0:
+        r+=1
+        c=0
+
 root.mainloop()
