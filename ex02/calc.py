223db2dd256a4acc7997a805fdedb7b95d528371
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-04 14:44:50 +0900

    4,テキスト入力欄

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,7 +10,10 @@ def button_click(event):
     txt = btn["text"]
     tkm.showinfo(txt,f"[{txt}]のボタンをクリック")
 
-r, c = 0, 0
+entry = tk.Entry(font=("Times New Roman",40),width=10,justify='right')
+entry.grid(row=0,column=0,columnspan=3)
+
+r, c = 1, 0
 for i, num in enumerate(range(9,-1,-1),1):
     button=tk.Button(root, text=f"{num}",font=("Ricty Diminished",30),width=4,height=2)
     button.grid(row = r, column = c)
