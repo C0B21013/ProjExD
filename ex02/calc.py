46a76d46735aa0b3772dbcc2eb3e1e8a046d5329
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-04 14:54:57 +0900

    6,＋追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.title("calc")
 root.geometry("300x500")
 
-def button_click(event):
+def num_click(event):
     btn = event.widget
     txt = btn["text"]
     entry.insert(tk.END,txt)
@@ -14,11 +14,15 @@ def button_click(event):
 entry = tk.Entry(font=("Times New Roman",40),width=10,justify='right')
 entry.grid(row=0,column=0,columnspan=3)
 
+button = tk.Button(text="+",font=("Ricty Diminished",30),width=4,height=2)
+button.bind("<1>",num_click)
+button.grid(row = 4, column = 1)
+
 r, c = 1, 0
 for i, num in enumerate(range(9,-1,-1),1):
     button=tk.Button(root, text=f"{num}",font=("Ricty Diminished",30),width=4,height=2)
     button.grid(row = r, column = c)
-    button.bind("<1>",button_click)
+    button.bind("<1>",num_click)
     c +=1 
     if i%3==0:
         r+=1
