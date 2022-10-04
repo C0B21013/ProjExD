7ffb5ff71eda9c97bbe8e002a468d3d4cc4c9fdb
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-04 15:28:40 +0900

    7,イコール追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,12 +11,22 @@ def num_click(event):
     entry.insert(tk.END,txt)
     #tkm.showinfo(txt,f"[{txt}]のボタンをクリック")
 
+def equal_click(event):
+    eq=entry.get()
+    res=eval(eq)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,res)
+
 entry = tk.Entry(font=("Times New Roman",40),width=10,justify='right')
 entry.grid(row=0,column=0,columnspan=3)
 
 button = tk.Button(text="+",font=("Ricty Diminished",30),width=4,height=2)
 button.bind("<1>",num_click)
 button.grid(row = 4, column = 1)
+
+button = tk.Button(text="=",font=("Ricty Diminished",30),width=4,height=2)
+button.bind("<1>",equal_click)
+button.grid(row = 4, column = 2)
 
 r, c = 1, 0
 for i, num in enumerate(range(9,-1,-1),1):
