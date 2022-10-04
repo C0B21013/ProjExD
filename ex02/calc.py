15de1c8cbf58d6ebafb68d8c281edc975ef06e8d
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-04 14:38:51 +0900

    3,クリック反応

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,10 +5,16 @@ root = tk.Tk()
 root.title("calc")
 root.geometry("300x500")
 
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt,f"[{txt}]のボタンをクリック")
+
 r, c = 0, 0
 for i, num in enumerate(range(9,-1,-1),1):
     button=tk.Button(root, text=f"{num}",font=("Ricty Diminished",30),width=4,height=2)
     button.grid(row = r, column = c)
+    button.bind("<1>",button_click)
     c +=1 
     if i%3==0:
         r+=1
