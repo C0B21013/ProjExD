d215eddb3d3d02866807c8a75af320fec8421f87
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-18 14:21:26 +0900

    4

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500,height=900,bg="black")
     canv.pack()
     tori = tk.PhotoImage(file="C:/Users/C0B21013/Documents/ProjExD2022/fig/3.png")
-    cx,cy =300,400
+    cx,cy = 300, 400 #効果トンの座標
     canv.create_image(cx,cy,image=tori,tag="tori")
-    
+    key = "" #現在押されているキー
     root.mainloop()
