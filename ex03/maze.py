67d7785537e5dc00fc33ea562e3dbe4caf3812d7
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-18 14:27:23 +0900

    5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,12 +1,22 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    
     canv = tk.Canvas(root, width=1500,height=900,bg="black")
     canv.pack()
+
     tori = tk.PhotoImage(file="C:/Users/C0B21013/Documents/ProjExD2022/fig/3.png")
     cx,cy = 300, 400 #効果トンの座標
     canv.create_image(cx,cy,image=tori,tag="tori")
+
     key = "" #現在押されているキー
+    root.bind("<KeyPress>",key_down)
+
     root.mainloop()
