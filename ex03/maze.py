815055f181148d4c550f4cd58d40d47777ba18ff
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-18 14:18:50 +0900

    3

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canv = tk.Canvas(root, width=1500,height=900,bg="black")
     canv.pack()
-
+    tori = tk.PhotoImage(file="C:/Users/C0B21013/Documents/ProjExD2022/fig/3.png")
+    cx,cy =300,400
+    canv.create_image(cx,cy,image=tori,tag="tori")
     
     root.mainloop()
