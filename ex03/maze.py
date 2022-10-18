6499d81ed74314910f9e711d29a6c0a91fc581bc
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-18 16:47:00 +0900

    演習

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,13 @@
+from ast import Delete
 from calendar import c
 import tkinter as tk
 import maze_maker as mm
 
+def count_up():
+    global tmr
+    tmr = tmr+1
+    label["text"]=tmr
+    root.after(1000,count_up)
 
 def key_down(event):
     global key
@@ -16,14 +22,18 @@ def key_up(event):
 def main_proc():
     global mx, my
     global cx, cy
-    
+    global tori
     if key =="Up":
+        tori = tk.PhotoImage(file="fig/5.png")
         my -= 1
     if key =="Down":
+        tori = tk.PhotoImage(file="fig/5.png")
         my += 1
     if key =="Left":
+        tori = tk.PhotoImage(file="fig/0.png")
         mx -= 1
     if key =="Right":
+        tori = tk.PhotoImage(file="fig/2.png")
         mx += 1
 
     if maze_list[my][mx]==0:
@@ -38,22 +48,31 @@ def main_proc():
             mx += 1
         if key =="Right":
             mx -= 1
-
-    canv.coords("tori",cx,cy)
-    root.after(100,main_proc)
+        tori = tk.PhotoImage(file="fig/8.png")
+    
+    
+    canv.delete("tori")
+    canv.create_image(cx,cy,image=tori,tag="tori")
+    #canv.coords("tori",cx,cy)
+    root.after(150,main_proc)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
+    label = tk.Label(root,font=("",15))
+    label.pack()
+    tmr = 0
+    root.after(1000, count_up)
+
     canv = tk.Canvas(root, width=1500,height=900,bg="black")
     canv.pack()
 
     maze_list= mm.make_maze(15,9)
     mm.show_maze(canv,maze_list)
-
-    tori = tk.PhotoImage(file="C:/Users/C0B21013/Documents/ProjExD2022/fig/3.png")
+    
+    tori = tk.PhotoImage(file="fig/0.png")
     mx, my = 1, 1
     cx, cy = mx*100 + 50, my*100 +50
     canv.create_image(cx,cy,image=tori,tag="tori")
