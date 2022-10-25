1b6ac5acbea9b440c0a3b7f70646c56426d8fe48
C0B21013 <c0b21013c2@edu.teu.ac.jp>
2022-10-25 17:28:38 +0900

    4限追加機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -15,6 +15,7 @@ def check_bound(obj_rct, scr_rct):
     if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
         tate = -1
     return yoko, tate
+    
 
 def main():
     #練習１
@@ -31,13 +32,22 @@ def main():
     tori_rct.center = 900,400
 
     #練習５
-    bomb_sfc = pg.Surface((20,20))
+    bomb_sfc = pg.Surface((50,50))
     bomb_sfc.set_colorkey((0,0,0))
-    pg.draw.circle(bomb_sfc,(255,0,0),(10,10),10)
+    pg.draw.circle(bomb_sfc,(5,5,5),(25,25),25)
+    pg.draw.line(bomb_sfc,(200,180,140),(25,20),(40,0),10)
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx, bomb_rct.centery, = randint(0,scrn_rct.width),randint(0,scrn_rct.height)
 
-    vx,vy= +1 ,+1
+    vx,vy= +2 ,+2
+
+    #アイテム
+    item_sfc = pg.Surface((50,50))
+    item_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(item_sfc,(255,255,0),(25,25),25)
+    item_rct = item_sfc.get_rect()
+    item_rct.center = randint(0,(scrn_rct.width-100)),randint(0,(scrn_rct.height-100))
+
 
     #練習２
     clock = pg.time.Clock()
@@ -48,30 +58,13 @@ def main():
             if event.type == pg.QUIT:
                 return
         
-        #練習４
-        key_states = pg.key.get_pressed()
-        if key_states[pg.K_UP]:
-            tori_rct.centery -= 1
-        if key_states[pg.K_DOWN]:
-            tori_rct.centery += 1 
-        if key_states[pg.K_LEFT]: 
-            tori_rct.centerx -= 1
-        if key_states[pg.K_RIGHT]:
-            tori_rct.centerx += 1
-        
-        yoko, tate = check_bound(tori_rct, scrn_rct)
-        if yoko == -1:
-            if key_states[pg.K_LEFT]: 
-                tori_rct.centerx += 1
-            if key_states[pg.K_RIGHT]:
-                tori_rct.centerx -= 1
-        if tate == -1:
-            if key_states[pg.K_UP]: 
-                tori_rct.centery += 1
-            if key_states[pg.K_DOWN]:
-                tori_rct.centery -= 1
+        #マウス操作
+        mouse_states = pg.mouse.get_pos()
+        tori_rct.center = mouse_states
+    
 
         scrn_sfc.blit(tori_sfc,tori_rct)
+        scrn_sfc.blit(item_sfc,item_rct)
         
         #練習６
         yoko, tate = check_bound(bomb_rct, scrn_rct)
@@ -80,8 +73,31 @@ def main():
         bomb_rct.move_ip(vx,vy)
         scrn_sfc.blit(bomb_sfc,bomb_rct)
 
-        if tori_rct.colliderect(bomb_rct):
+        if tori_rct.colliderect(item_rct):
+            fonto = pg.font.Font(None,80)
+            txt = fonto.render(str("TEST"),True,(255,0,0))
+            scrn_sfc.blit(txt,(800,400))
+            
+            pg.display.update()
+            clock = pg.time.Clock()
+            clock.tick(1)
             return
+
+        if tori_rct.colliderect(bomb_rct):
+
+            #ゲームオーバー
+            scrn_sfc.blit(bg_sfc,bg_rct)
+            tori_sfc = pg.image.load("fig/8.png")
+            tori_sfc = pg.transform.rotozoom(tori_sfc,0,2.0)
+            scrn_sfc.blit(tori_sfc,tori_rct)
+            fonto = pg.font.Font(None,80)
+            txt = fonto.render(str("GAMEOVER"),True,(255,0,0))
+            scrn_sfc.blit(txt,(800,400))
+            pg.display.update()
+            clock = pg.time.Clock()
+            clock.tick(0.5)
+            return
+            
         
         pg.display.update()
         clock.tick(1000)
